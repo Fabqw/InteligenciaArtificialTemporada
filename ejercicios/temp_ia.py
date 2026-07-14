82626851b31dfc9a22b65cd4198d7631ca6fa2fb
Fabqw <ffquispe8@umsa.bo>
2026-07-14 18:28:03 -0400

    fix

--- ejercicios/temp_ia.py
+++ ejercicios/temp_ia.py
@@ -9,8 +9,8 @@ df = pd.read_csv('data1.csv', skiprows=2)
 df.columns = ['Semana', 'Mundial', 'Televisor']
 
 # Limpiar datos( eliminar los caracteres raros como <1 que a veces eexporta google) 
-df['Mundial'] = pd.to_numeric(df['Mundial'].astype(str).str.replace('<'), errors='coerce').fillna(0)
-df['Televisor'] = pd.to_numeric(df['Televisor'].astype(str).str.replace('<'), errors='coerce').fillna(0)
+df['Mundial'] = pd.to_numeric(df['Mundial'].astype(str).str.replace('<', ''), errors='coerce').fillna(0)
+df['Televisor'] = pd.to_numeric(df['Televisor'].astype(str).str.replace('<', ''), errors='coerce').fillna(0)
 
 # Calcualr las correlaciones matematicas
 correlation = df['Mundial'].corr(df['Televisor'])
